7af7a947fdd868ca730d564ef3f0210dbf03bdd2
jmikestrong <jmikestrong@gmail.com>
2021-04-11 15:49:42 -0400

    customer_feedback start

---
+++ customer_feedback.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+#Shopping Experience
+
+echo " $VARNAME, Please rate your CULT OF APPLE shopping experience" 
+
+echo  "On a scale of 1 or 5, 1 being a poor experience; 5 potential repeat busin$
+
+for 1 in How shall we better serve you, {$VARNAME} when you come again? 
+do
+      echo " Poor Experience, $1"
+done 
