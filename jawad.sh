faf2b0217a0e25741966a0b419a137796eeaca4c
jahmed51 <j_ahmed51@yahoo.com>
2021-04-10 21:41:21 -0400

    changes

--- jawad.sh
+++ jawad.sh
@@ -1,108 +1,53 @@
-#!/bin/bash
-#Welcome
-
-echo "     Welcome to Apple.com"
-echo "-----------------------------------"
-echo "-----------------------------------"
-echo "-----------------------------------"
-
-
-echo " What product would you like to buy today ?"
-
-
-echo "------------------------------------"
-
-#DEVICEOPTIONS
-
-DEVICEOPTION1=$Ipad
-DEVICEOPTION2=$Iphone
-DEVICEOPTION3=$Mac
-
-echo "1. Ipad"
-echo "2. Iphone"
-echo "3. Macbook"
-
-read -p " Please select your device [1-3]: "DEVICEOPTION
-if [ $DEVICEOPTION1 == $Ipad ]; then
-echo " You have selected an Ipad"
-elif [ $DEVICEOPTION2 == $Iphone  ]; then
-echo " You have selected an Iphone"
-elif [  $DEVICEOPTION3 == $Mac  ]; then
-echo "You have selected Macbook"
-else 
-     echo "no other options found"
-fi
+																	#!/bin/bash
+#Welcome----------------------------------
 
 
 
 
-echo "------------------------------------"
-#DEVICEMODELS
-#Ipad
 
-DEVICEMODEL1=$Ipad
-DEVICEMODEL2=$Ipadmini
-DEVICEMODEL3=$IpadPro
 
-echo "1. Ipad"
-echo "2. Ipadmini"
-echo "3. IpadPro"
-read -p "please select your device [1-3]:" DEVICEMODEL
-
-if  [ $DEVICEMODEL1 == $Ipad ]; then
-echo " you have selected an Ipad"
-elif [ $DEVICEMODEL2 == $Ipadmini ]; then
-echo " you have selected an Ipadmini"
-elif [ $DEVICEMODEL3 == $IpadPro ]; then
-echo " you have selected an Ipad"
-else
- echo " no other Models found "
+echo  "  --------------------"
+echo  "   Welcome to Apple"
+echo  "  --------------------"
 
 
 
+#Question--------------------------------
 
 
 echo "-----------------------------------"
+echo " what product would you like to buy?"
+echo "-----------------------------------"
 
-#IPHONE
-DEVICEMODEL1=$Iphone12
-DEVICEMODEL2=$Iphone12Pro
-DEVICEMODEL3=$Iphone12ProMax
-echo " Apple offers 3 models currently: "
-echo "1. Iphone12 "
-echo "2. Iphone12Pro"
-echo "3. Iphone12ProMax"
-echo "4. Return to previous menu"
-read -p "Click on your choice :" DEVICEMODEL
-if [ $DEVICEMODEL1   == $Iphone12 ]; then
-echo " You have selected an Iphone12"
-elif [ $DEVICEMODEL2 == $Iphone12pro ]; then
-echo " You have selected an Iphone12Pro"
-elif [ $DEVICEMODEL3 == $Iphone12ProMax ]; then
-echo " You have selected an Iphone12ProMax"
-else 
-echo " no other options found"
-fi
+#different device options----------------------
+
+options=(Ipad Iphone Mac)
+
+num=0
+
+for i in "${options[@]}"
+do
+    ((num++))
+    echo "$num$i"
+done
 
 
+read -p "Please select your device: " name
 
-echo "----------------------------------------"
+case $name in
+ ipad)
+   echo -n "You have selected an Ipad"
+   ;;
+ Iphone)
+   echo -n "You have selected an Iphone"
+   ;;
+  Mac)
+   echo -n "You have selected a Mac"
+   ;;
+   *)
+   echo -n "no other options found"
+   ;;
+esac
 
-DEVICEMODEL1=$Macbookair
-DEVICEMODEL2=$MacbookPro
-DEVICEMODEL3=$IMac
-
-echo "1. Macbookair"
-echo "2. MacbookPro"
-echo "3. IMac"
-read -p "Please select your device [1-3]:"DEVICEMODEL 
-if [ $DEVICEMODEL1 == $Macbookair ]; then
-echo " You have selected a Macbook air"
-elif  [  $DEVICEMODEL2 == $MacbookPro ]; then
-echo " You have selected a MacbookPro"
-elif [ $DEVICEMODEL3 == $MacbookPro ]; then
-echo " You have selected an IMac"
-else 
-echo " no other options found"
-fi
+#Iphone Options
 
