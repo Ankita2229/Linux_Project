09371dadb12e299c43790d755148cb7e77f38313
nasirahmed8909 <nahmed8909@gmail.com>
2021-04-13 15:56:27 -0400

    preferrence for apple devices

--- applestore.sh
+++ applestore.sh
@@ -1,6 +1,85 @@
 #!/bin/bash
 
-#IPHONE
+select_size () {
+    size=(12  pro pro\ max)
+
+    n=0
+    for i in "${size[@]}"
+    do
+
+      ((n++))
+      echo "$n $i"
+      done
+}
+
+
+
+
+select_color () {
+    color=(Black White Purple)
+
+n=0
+for i in "${color[@]}"
+do
+   ((n++))
+    echo "$n $i"
+    done
+}
+
+
+select_storage () {
+      storage=(128GB 256GB 512GB)
+
+n=o
+for i in "${storage[@]}"
+
+do 
+
+ ((n++))
+  echo "$n $i"
+  done
+
+}
+
+
+
+select_size
+read -p " what size do you like to get?"  Selected_Size
+select_color
+read -p "what color do you like to get?" Selected_Color
+select_storage
+read -p "what storage do you like to get?" Selected_Storage
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##IPHONE
 SELECTEDSIZE=0
 SELECTEDCOLOR=0
 SELECTEDSTORAGE=0
@@ -15,6 +94,7 @@ function showAvailablesizes
     echo "3. Return to the previous menu"
     read -p  "Enter your choice:" SELECTEDSIZE  
   }
+showAvailablesizes
   
   
   
@@ -24,7 +104,7 @@ function showAvailablesizes
     MESSAGE =""
     case $SELECTEDSIZE in
     
-      if [ $SELECTEDSIZE -eq 1 ]
+      if " [ $SELECTEDSIZE -eq 1 ]"
       then SELECTEDSIZE=iPhone pro
       else
            SELECTEDSIZE=iphone pro max
