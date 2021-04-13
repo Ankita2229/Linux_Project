db12ec4cedae880d805ae69ee863dceccb1e78d3
jmikestrong <jmikestrong@gmail.com>
2021-04-13 17:08:44 -0400

    newcode

--- customer_feedback.sh
+++ customer_feedback.sh
@@ -4,10 +4,13 @@
 #Shopping Experience
 
 echo " $VARNAME, Please rate your CULT OF APPLE shopping experience" 
+echo  "On a scale of 1 or 5, 1 being a poor experience; 5 potential repeat business and referral "
 
-echo  "On a scale of 1 or 5, 1 being a poor experience; 5 potential repeat busin$
-
-for 1 in How shall we better serve you, {$VARNAME} when you come again? 
+until [ "1" == "$How shall we better serve you, $VARNAME when you come again?"]
 do
-      echo " Poor Experience, $1"
-done 
+echo "How shall we better serve you, $VARNAME when you come again?"
+break
+until [ "5" == "$Thank you $VARNAME come again."]
+do 
+echo "Thank you $VARMANE come again."
+done
