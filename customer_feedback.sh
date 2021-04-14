574ff5de80acc7003b4d1d0b261a544e540c542d
jmikestrong <jmikestrong@gmail.com>
2021-04-14 15:25:54 -0400

    done!

--- customer_feedback.sh
+++ customer_feedback.sh
@@ -3,14 +3,15 @@
 
 #Shopping Experience
 
-echo " $VARNAME, Please rate your CULT OF APPLE shopping experience" 
-echo  "On a scale of 1 or 5, 1 being a poor experience; 5 potential repeat business and referral "
-
-until [ "1" == "$How shall we better serve you, $VARNAME when you come again?"]
-do
+echo " $VARNAME, Please rate your CULT OF APPLE shopping experience"
 echo "How shall we better serve you, $VARNAME when you come again?"
-break
-until [ "5" == "$Thank you $VARNAME come again."]
-do 
-echo "Thank you $VARMANE come again."
+
+read -p "On a scale of 1 or 5, 1 being a poor experience; 5 potential repeat business plus referral"  rate
+until [[ $rate == [0-5] ]]
+do
+echo "CHOOSE EITHER 1 or 5"
+read -p "On a scale of 1 or 5, 1 being a poor experience; 5 potential repeat business plus referral"  rate
 done
+echo "Thank you for your time and feedback today $VARMANE, come again, please."
+
+
